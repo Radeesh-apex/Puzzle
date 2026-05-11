e250cdd95e8f05fe6568d9c28da141aeda92a9b9
Radeesh-apex <rharijana@apexsystems.com>
2026-05-11 12:53:05 +0530

    Add initial configuration data in config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+// config.js
+const configData = {
+    "auth": {
+        "alpha": "MTIzNA==", // 1234
+        "bravo": "NTY3OA==",  // 5678
+	"bravo1": "NTY3OA=="  // 5678
+
+    },
+    "data": {
+        "alpha": [
+		{
+                "q": "Level 1: Visual Identification", 
+                "h": "Who is this scientist?", 
+                "img": "https://upload.wikimedia.org/wikipedia/commons/d/d3/Albert_Einstein_Head.jpg", // Add URL here
+                "a": "ZWluc3RlaW4=", // Answer: einstein
+                "k": "RELATIVITY" 
+            },
+            { "q": "Level 1: The Number Code", "h": "What is 12 x 12?", "a": "MTQ0", "k": "ORION" },
+            { "q": "Level 2: Hardware", "h": "The 'brain' of the computer (3 letters)", "a": "Y3B1", "k": "VORTEX" },
+            { "q": "Level 3: Logic", "h": "I have keys but no locks. I have space but no room.", "a": "a2V5Ym9hcmQ=", "k": "NULL" }
+        ],
+        "bravo": [
+            { "q": "Level 1: Geography", "h": "Which country has the Eiffel Tower?", "a": "ZnJhbmNl", "k": "CYBER" },
+            { "q": "Level 2: Math", "h": "Result of (50 / 2) + 5", "a": "MzA=", "k": "PULSE" },
+            { "q": "Level 3: Riddle", "h": "What gets wetter as it dries?", "a": "dG93ZWw=", "k": "LINK" }
+        ],
+"bravo1": [
+            { "q": "Level 1: Geography", "h": "Which country has the Eiffel Tower?", "a": "ZnJhbmNl", "k": "CYBER" },
+            { "q": "Level 2: Math", "h": "Result of (50 / 2) + 5", "a": "MzA=", "k": "PULSE" },
+            { "q": "Level 3: Riddle", "h": "What gets wetter as it dries?", "a": "dG93ZWw=", "k": "LINK" }
+        ]
+
+    }
+};
